9f1ae9cf8856147f2f862fdec35c3044d3bb386a
sava-rachynskyi <savarachynskyi@gmail.com>
2026-08-28 19:31:00 +0200

    init commit

---
+++ src/test/java/com/savarachynskyi/vault/EphemeralSecretVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savarachynskyi.vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EphemeralSecretVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
