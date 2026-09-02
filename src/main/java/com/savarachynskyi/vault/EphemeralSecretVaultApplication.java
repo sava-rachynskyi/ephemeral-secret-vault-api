4c956e1bd0d395ebb8f083b903e90b3e96451244
sava-rachynskyi <savarachynskyi@gmail.com>
2026-09-02 23:55:41 +0200

    fix(config): relocate main class to root package and update db port

--- src/main/java/com/savarachynskyi/vault/EphemeralSecretVaultApplication.java
+++ src/main/java/com/savarachynskyi/vault/EphemeralSecretVaultApplication.java
@@ -1,4 +1,4 @@
-package com.savarachynskyi.vault.config;
+package com.savarachynskyi.vault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
